3c20227feacaa97f403d89700ce3a0c6c4d7ec88
Rishabh Sharma <rishabh.s2203@outlook.com>
2025-06-10 08:59:36 +0530

    added further functionalities

--- app/src/main/java/ticket/booking/entities/Ticket.java
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -9,4 +9,67 @@ public class Ticket {
     private String destination;
     private Date date;
     private Train train;
+
+    public Ticket(String ticketId, String userId, String source, String destination, Date date, Train train) {
+        this.ticketId = ticketId;
+        this.userId = userId;
+        this.source = source;
+        this.destination = destination;
+        this.date = date;
+        this.train = train;
+    }
+
+    public Ticket() {}
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Train getTrain() {
+        return train;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
+    }
+
+    public String getTicketInfo() {
+        return String.format("Ticked id: %s belongs to User %s from %s to %s on %s", ticketId, userId, source, destination, date);
+    }
 }
--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,11 +1,72 @@
 package ticket.booking.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import ticket.booking.entities.User;
+import ticket.booking.util.UserServiceUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 public class UserBookingService {
-    private User user;
-    public UserBookingService(User user) {
+    private final User user;
+    private List<User> userList;
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private static final String USERS_PATH = "app/src/main/java/ticket/booking/localDb/users.json";
+    public UserBookingService(User user) throws IOException{
         this.user = user;
+        objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        loadUsers();
+    }
+    private void loadUsers() throws IOException{
+        userList = objectMapper.readValue(new File(USERS_PATH), new TypeReference<List<User>>() {});
+    }
+
+    public Boolean loginUser() {
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equalsIgnoreCase(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1) {
+        try {
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        } catch (IOException e) {
+            return Boolean.FALSE;
+        }
+    }
+
+    private void saveUserListToFile() throws IOException {
+        File userFile = new File(USERS_PATH);
+        objectMapper.writeValue(userFile, userList);
+    }
+
+    private void fetchBooking() {
+        user.printTickets();
+    }
+
+    private Boolean cancelBooking(String ticketId) throws IOException {
+        if (ticketId == null || ticketId.isEmpty()) {
+            System.out.println("Enter a valid ticked id");
+            return Boolean.FALSE;
+        }
+
+        boolean isRemoved = user.getTicketsBooked().removeIf(ticket -> ticket.getTicketId().equals(ticketId));
+        if (isRemoved) {
+            saveUserListToFile();
+            System.out.println("Ticket with id " + ticketId + " has been cancelled");
+            return true;
+        } else {
+            System.out.println("No ticket found with ticketId: " + ticketId);
+            return false;
+        }
     }
 
 
